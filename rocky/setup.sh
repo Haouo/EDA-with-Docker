c0ad99da0f7d2a86afb31429ada31273aedda0d3
Haouo <q18131500@gs.ncku.edu.tw>
2025-08-08 02:06:40 +0800

    Make the workflow of running EDA remotely on ubuntu container smoother

--- rocky/setup.sh
+++ rocky/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PUBKEY_FILE="/home/rocky/.ssh/shared_ssh_key/ubuntu_id_rsa.pub"
-TARGET_AUTH_KEY_FILE="/home/rocky/.ssh/authorized_keys"
+PUBKEY_FILE="/home/aislab/.ssh/shared_ssh_key/ubuntu_id_rsa.pub"
+TARGET_AUTH_KEY_FILE="/home/aislab/.ssh/authorized_keys"
 
 if [ -f "$PUBKEY_FILE" ]; then
     cat "$PUBKEY_FILE" >> "$TARGET_AUTH_KEY_FILE"
-    chown -R rocky:rocky /home/rocky/.ssh
+    chown -R aislab:aislab /home/aislab/.ssh
     chmod 600 "$TARGET_AUTH_KEY_FILE"
 fi
 
